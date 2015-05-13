da13ab812ddf8ce4814fa2eaf960f6db60910817
Truerror <uitiong@live.com>
2015-05-13 12:21:25 +0800

    Made a new interface for scenes.

---
+++ PeaShooterGame/Utils/IScene.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PeaShooterGame.Utils
+{
+    public interface IScene
+    {
+        void Initialize();
+        void LoadContent();
+        void Update(GameTime gameTime);
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
